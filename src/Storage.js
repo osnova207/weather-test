420cf2a2286567c2616db83769f4c9c9cc4d2251
Osnova207 <osnova207@gmail.com>
2020-10-25 22:24:39 +0200

    added local storage service

---
+++ src/Storage.js
@@ -0,0 +1,22 @@
+export default class Storage {
+
+    static set(key, obj) {
+        window.localStorage && window.localStorage.setItem(key, JSON.stringify(obj));
+    }
+
+    static get(key, defaultValue = null) {
+        let data = defaultValue;
+        const objJSON = window.localStorage && window.localStorage.getItem(key);
+        if (typeof objJSON === 'string') {
+            try {
+                data = JSON.parse(objJSON)
+            } catch (e) {
+            }
+        }
+        return data;
+    }
+
+    static remove(key) {
+        window.localStorage && window.localStorage.removeItem(key)
+    }
+}
